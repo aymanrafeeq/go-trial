b2293dce7ce4a2af7c9b96a379952b1213d37440
ayman <aymanrafeeque419@gmail.com>
2025-10-09 10:31:03 +0530

    added main file

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(abbreviate("Central Processing Unit"))
+	fmt.Println(fizzbizz(20))
+	fmt.Println(frequency("Ayman"))
+	fmt.Println(palindrome("malayalam"))
+	fmt.Println(pangram("the quick brown fox jumps over the lazy dog"))
+}
